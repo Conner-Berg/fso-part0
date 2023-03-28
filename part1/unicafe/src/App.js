8043edf80ffa645879354fb8ffa96a54aec7e9a0
Conner-Berg <connerwberg@gmail.com>
2023-03-28 13:36:38 -0400

    Finish 1.7 button functionality and NaN checks

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Button = (props) => {
-	return <button>{props.value}</button>;
+	return <button onClick={props.handleClick}>{props.value}</button>;
 };
 
 const App = () => {
@@ -10,21 +10,28 @@ const App = () => {
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
 
+	const average = (good - bad) / (good + neutral + bad);
+	const positive = (good / (good + neutral + bad)) * 100;
+
+	const addGood = () => setGood(good + 1);
+	const addNeutral = () => setNeutral(neutral + 1);
+	const addBad = () => setBad(bad + 1);
+
 	return (
 		<div>
 			<div className="header">give feedback</div>
 			<div>
-				<Button value="good" />
-				<Button value="neutral" />
-				<Button value="bad" />
+				<Button handleClick={addGood} value="good" />
+				<Button handleClick={addNeutral} value="neutral" />
+				<Button handleClick={addBad} value="bad" />
 			</div>
 			<div className="header">statistics</div>
-			<div>good </div>
-			<div>neutral </div>
-			<div>bad </div>
-			<div>all </div>
-			<div>average </div>
-			<div>positive </div>
+			<div>good {good} </div>
+			<div>neutral {neutral} </div>
+			<div>bad {bad}</div>
+			<div>all {good + neutral + bad} </div>
+			<div>average {isNaN(average) ? 0 : average} </div>
+			<div>positive {isNaN(positive) ? 0 : positive} % </div>
 		</div>
 	);
 };
