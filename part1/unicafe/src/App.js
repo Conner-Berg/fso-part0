af30eb2942b2e7f64795ba9198185bd9928b2a10
Conner-Berg <connerwberg@gmail.com>
2023-03-28 12:55:52 -0400

    Add basic structure from 1.7 image

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,32 @@
 import { useState } from "react";
 
+const Button = (props) => {
+	return <button>{props.value}</button>;
+};
+
 const App = () => {
 	// save clicks of each button to its own state
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
 
-	return <div>code here</div>;
+	return (
+		<div>
+			<div className="header">give feedback</div>
+			<div>
+				<Button value="good" />
+				<Button value="neutral" />
+				<Button value="bad" />
+			</div>
+			<div className="header">statistics</div>
+			<div>good </div>
+			<div>neutral </div>
+			<div>bad </div>
+			<div>all </div>
+			<div>average </div>
+			<div>positive </div>
+		</div>
+	);
 };
 
 export default App;
