e6d3ccb2f613cb9e4a48645fa83b42e2cc7da497
Conner-Berg <connerwberg@gmail.com>
2023-03-31 17:05:47 -0400

    Finish 2.8, newNumber state and handler

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 const App = () => {
 	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
 	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
 
 	const addPerson = (event) => {
 		event.preventDefault();
@@ -13,14 +14,15 @@ const App = () => {
 
 		const personObject = {
 			name: newName,
+			number: newNumber,
 		};
 		setPersons(persons.concat(personObject));
 		setNewName("");
+		setNewNumber("");
 	};
 
-	const handlePersonChange = (event) => {
-		setNewName(event.target.value);
-	};
+	const handlePersonChange = (event) => setNewName(event.target.value);
+	const handleNumberChange = (event) => setNewNumber(event.target.value);
 
 	return (
 		<div>
@@ -31,13 +33,19 @@ const App = () => {
 					<input onChange={handlePersonChange} value={newName} />
 				</div>
 				<div>
+					number:{" "}
+					<input onChange={handleNumberChange} value={newNumber} />
+				</div>
+				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
 			<div>
 				{persons.map((person) => (
-					<p key={person.name}>{person.name}</p>
+					<p key={person.name}>
+						{person.name} {person.number}{" "}
+					</p>
 				))}
 			</div>
 		</div>
