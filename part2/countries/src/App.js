d7e6fe9be622d9e86335239bb1c82ae362f90114
Conner-Berg <connerwberg@gmail.com>
2023-04-04 13:13:20 -0400

    Add effect, and listing countries functionality

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,8 +5,24 @@ const App = () => {
 	const [countries, setCountries] = useState([]);
 	const [search, setSearch] = useState("");
 
+	useEffect(() => {
+		axios.get("https://restcountries.com/v3.1/all").then((response) => {
+			setCountries(response.data);
+		});
+	}, []);
+
+	const filteredCountries = countries.filter((country) =>
+		country.name.common.toLowerCase().includes(search.toLowerCase())
+	);
+
+	const displayResult = () => {
+		return filteredCountries.map((country) => (
+			<div key={country.cca3}>{country.name.common}</div>
+		));
+	};
+
 	return (
-		<div>
+		<>
 			<div>
 				Find countries{" "}
 				<input
@@ -15,8 +31,8 @@ const App = () => {
 					onChange={(event) => setSearch(event.target.value)}
 				/>
 			</div>
-			{/* displayResult */}
-		</div>
+			<div>{displayResult()}</div>
+		</>
 	);
 };
 
