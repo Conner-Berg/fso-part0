46150707c653e62a2b4fedcf5307eaf1689f3734
Conner-Berg <connerwberg@gmail.com>
2023-04-04 12:53:58 -0400

    Add imports, states, basic form template

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,5 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const App = () => {
-	return;
+	const [countries, setCountries] = useState([]);
+	const [search, setSearch] = useState("");
+
+	return (
+		<div>
+			<div>
+				Find countries{" "}
+				<input
+					type="text"
+					value={search}
+					onChange={(event) => setSearch(event.target.value)}
+				/>
+			</div>
+			{/* displayResult */}
+		</div>
+	);
 };
 
 export default App;
