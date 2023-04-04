25c6c5f2f8386a9161bd16ed634bdbc546d22adb
Conner-Berg <connerwberg@gmail.com>
2023-04-04 15:56:52 -0400

    Finish 2.20, weather API for country capital

--- part2/countries/src/components/CountryDetails.js
+++ part2/countries/src/components/CountryDetails.js
@@ -1,10 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const CountryDetails = ({ country }) => {
+	const [weather, setWeather] = useState(null);
+	const api_key = process.env.REACT_APP_API_KEY;
+	const capital = country.capital[0];
 	const languages = Object.values(country.languages);
+
+	useEffect(() => {
+		axios
+			.get(
+				`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}&units=metric`
+			)
+			.then((response) => {
+				setWeather(response.data);
+			})
+			.catch((error) => {
+				console.error("Error fetching weather data:", error);
+			});
+	}, [capital, api_key]);
+
 	return (
 		<div>
 			<h1>{country.name.common}</h1>
-			<div>capital {country.capital[0]}</div>
-			<p>area {country.area}</p>
+			<div>capital {capital}</div>
+			<div>area {country.area}</div>
 			<h3>languages:</h3>
 			<ul>
 				{languages.map((language) => (
@@ -16,6 +36,19 @@ const CountryDetails = ({ country }) => {
 				alt={`${country.name.common} flag`}
 				style={{ width: "150px" }}
 			/>
+			{weather && (
+				<div>
+					<h3>Weather in {capital}</h3>
+					<p>temperature {weather.main.temp.toFixed(2)} Celsius</p>
+					<img
+						src={`https://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+						alt={weather.weather[0].description}
+						width="100px"
+						height="100px"
+					/>
+					<p>wind {weather.wind.speed} m/s</p>
+				</div>
+			)}
 		</div>
 	);
 };
